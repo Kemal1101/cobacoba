39417cbd9bc033471afb04bf18a76ecfd8e497b7
Kemal1101 <lataxa.11@gmail.com>
2023-09-07 10:00:59 +0700

    commit

--- Tulisan.java
+++ Tulisan.java
@@ -1,6 +1,9 @@
-public class tulisan {
+public class Tulisan {
     public static void main(String[] args) {
         System.out.println("apa apaan");
-        
+        String minaName = "Sharon";
+        System.out.println(minaName);
+        int x = 15;
+        System.out.println(x);
     }
 }
