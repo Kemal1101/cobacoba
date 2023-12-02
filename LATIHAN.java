6d5b2faa452c4a42230ef430ac9f7055f1659976
Kemal1101 <lataxa.11@gmail.com>
2023-12-02 11:09:43 +0700

    update

--- LATIHAN.java
+++ LATIHAN.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
 public class LATIHAN {
-    static int tambah (int n){
-        if (n <= 1){
-            return (n);
-        }else{
-            return tambah(n -1) + tambah(n-2);
+    static float investasi (int tahun, float uang, float total){
+        if (tahun >= 1){
+             total += total * 0.117;
+             return (investasi(tahun - 1, uang, total)); 
+        }else {
+            return (total); 
         }
         
-    }    
-    public static void main(String[] args) {
-        int n = 12;
-        for (int i = 0 ; i <= n ; i++){
-            System.out.print(tambah(i) + " ");
-        }
     }
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan uang yang anda tabung ke emas: ");
+        float uang = sc.nextFloat();
+        float total = uang;
+        System.out.print("Masukkan berapa tahun anda ingin menabung: ");
+        int tahun = sc.nextInt();
+
+        System.out.print("Total uang anda adalah " + investasi(tahun, uang, total));
+    }
 }
